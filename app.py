c872c3feecb8ea0a36eb5a4385a530099a2e6791
Anirudhi Thanvi <42400848+ani-thanvi@users.noreply.github.com>
2021-02-03 02:26:17 +0530

    index update

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ from keras.preprocessing import image
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+#from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH = 'models/model_resnet.h5'
+MODEL_PATH = 'tomato_cnn.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
