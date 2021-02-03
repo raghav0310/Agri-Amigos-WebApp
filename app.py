b2da4558df9923c057860554f39c52f7bcab785c
Anirudhi Thanvi <42400848+ani-thanvi@users.noreply.github.com>
2021-02-03 11:01:17 +0530

    index update

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ import numpy as np
 # Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
-from keras.preprocessing import image
+from tensorflow.keras.preprocessing import image
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-#from gevent.pywsgi import WSGIServer
+from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
@@ -59,25 +59,27 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
+    try :
+        if request.method == 'POST':
+            # Get the file from post request
+            f = request.files['file']
 
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
+            # Save the file to ./uploads
+            basepath = os.path.dirname(__file__)
+            file_path = os.path.join(
+                basepath, 'uploads', secure_filename(f.filename))
+            f.save(file_path)
 
-        # Make prediction
-        preds = model_predict(file_path, model)
+            # Make prediction
+            preds = model_predict(file_path, model)
 
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        result = str(pred_class[0][0][1])               # Convert to string
-        return result
-    return None
+            # Process your result for human
+            # pred_class = preds.argmax(axis=-1)            # Simple argmax
+            pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+            result = str(pred_class[0][0][1])               # Convert to string
+            return result
+    except:
+        return json.dump({"error": "File formaat not correct"})
 
 
 if __name__ == '__main__':
