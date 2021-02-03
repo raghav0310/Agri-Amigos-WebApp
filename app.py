ff552c1c08b02a26cda0ed55886c84486b541aa6
Nitesh-Harit <65038072+Nitesh-Harit@users.noreply.github.com>
2021-02-03 14:39:41 +0530

    changes for model

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ MODEL_PATH = 'tomato_cnn.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary
+#model._make_predict_function()          # Necessary
 # print('Model loaded. Start serving...')
 
 # You can also use pretrained model from Keras
@@ -36,7 +36,7 @@ model._make_predict_function()          # Necessary
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
-def model_predict(img_path, model):
+def model_predict(img_path, models):
     img = image.load_img(img_path, target_size=(224, 224))
 
     # Preprocessing the image
@@ -48,7 +48,7 @@ def model_predict(img_path, model):
     # otherwise, it won't make correct prediction!
     x = preprocess_input(x, mode='caffe')
 
-    preds = model.predict(x)
+    preds = models.predict(x)
     return preds
 
 
@@ -60,28 +60,26 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
-    try :
-        if request.method == 'POST':
-            # Get the file from post request
-            f = request.files['file']
 
-            # Save the file to ./uploads
-            basepath = os.path.dirname(__file__)
-            file_path = os.path.join(
-                basepath, 'uploads', secure_filename(f.filename))
-            f.save(file_path)
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
 
-            # Make prediction
-            preds = model_predict(file_path, model)
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
 
-            # Process your result for human
-            # pred_class = preds.argmax(axis=-1)            # Simple argmax
-            pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-            result = str(pred_class[0][0][1])               # Convert to string
-            return result
-    except:
-        return json.dump({"error": "File formaat not correct"})
+        # Make prediction
+        preds = model_predict(file_path, model)
 
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
 
 if __name__ == '__main__':
     app.run(debug=True)
